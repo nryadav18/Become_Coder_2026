0a80f289594676bc0b8769d37ae8fb9632e2a500
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2024-01-07 23:00:46 +0530

    Update Prefix Sum.cpp

--- Prefix Sum.cpp
+++ Prefix Sum.cpp
@@ -1,37 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-    //Pre_Fix Sum using Map and Constant Space
+int main() {
+    //Sub Array range with given sum;
     int n;
     cin>>n;
     int arr[n];
     for (int i=0;i<n;i++){
         cin>>arr[i];
     }
-    int t;
-    cin>>t;
-    map<int,int>mp;
-    int pre_fix[n];
+    int target;
+    cin>>target;
+    vector<int>pre_fix(n,0);
     pre_fix[0] = arr[0];
     int sum = arr[0];
     for (int i=1;i<n;i++){
         sum += arr[i];
         pre_fix[i] = sum;
     }
+    map<int,vector<int>>mov;
     for (int i=0;i<n;i++){
-        if (pre_fix[i]==t){
-            cout << 0 << " " << i << " ";
+        if (pre_fix[i] == target){
+            cout << 0 << " " << i << endl;
+            mov[pre_fix[i]].push_back(i);
         }
-        else if (pre_fix[i]<t){
-            mp[pre_fix[i]] = i;
+        else if (pre_fix[i] < target){
+            mov[pre_fix[i]].push_back(i);
         }
         else{
-            int diff = pre_fix[i] - t;
-            if (mp[diff]!=-1){
-                cout << mp[diff]+1 << " " << i <<  endl;
+            int diff = pre_fix[i] - target;
+            if (mov[diff].size()==0){
+                mov[pre_fix[i]].push_back(i);
             }
             else{
-                mp[pre_fix[i]] = i;
+                for (auto it:mov[diff]){
+                    cout << it+1 << " " << i << endl; 
+                }
+                mov[pre_fix[i]].push_back(i);
             }
         }
     }
